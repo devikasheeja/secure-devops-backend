58987f8a1c16ac96473e8b65b1dd583172bff27e
_devikaS <devika.s@ibilglobal.com>
2026-03-18 17:31:46 +0000

    add vulnerability

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//  ADD THIS PART
+app.get("/search", (req, res) => {
+  const query = "SELECT * FROM users WHERE name = '" + req.query.name + "'";
+  res.send(query);
+});
+
 app.listen(3000, () => console.log("Running"));
